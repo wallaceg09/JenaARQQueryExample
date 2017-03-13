8504374a640333648682cbc774ccca09b856b98e
Glen Wallace <glen.wallace@praeses.com>
2017-03-13 16:41:30 -0500

    Initial commit

---
+++ src/main/java/com/praeses/semantic/example/JenaARQQueyExample.java
@@ -0,0 +1,29 @@
+package com.praeses.semantic.example;
+
+import org.apache.jena.query.ParameterizedSparqlString;
+import org.apache.jena.query.Query;
+import org.apache.jena.sparql.vocabulary.FOAF;
+
+/**
+ *
+ */
+public class JenaARQQueyExample {
+    public static Query getQuery() {
+        ParameterizedSparqlString sparqlString = new ParameterizedSparqlString("" +
+                "SELECT ?title\n" +
+                "WHERE { \n" +
+                "?character foaf:name ?characterName.\n" +
+                "?character foaf:title ?title.\n" +
+                "} ORDER BY ?title\n");
+
+        sparqlString.setNsPrefix("foaf", FOAF.getURI());
+
+        sparqlString.setLiteral("characterName", "Aegon I");
+
+        return sparqlString.asQuery();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getQuery());
+    }
+}
